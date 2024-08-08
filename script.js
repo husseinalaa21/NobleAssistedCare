f1b16f1bbd990bf2983810c4ec625ec93e164300
Hussein Alzuabidi <hussein.scs@icloud.com>
2024-08-08 17:39:09 -0400

    New

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-var main = "D";
+window.onscroll = function() {myFunction()};
+    
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+ var menu_list = document.getElementById("menu_list")
+ var mls = false
+
+ function openlist(element) {
+    element.classList.toggle('opened');
+    element.setAttribute('aria-expanded', element.classList.contains('opened'));
+    
+    if(mls == false){
+        menu_list.style.display = "block"
+        mls = true
+    } else {
+        menu_list.style.display = "none"
+        mls = false
+    }
+}
