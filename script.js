219c26eb27158403d74e935995ae9bd6dbe2021a
Hussein Alzuabidi <hussein.scs@icloud.com>
2024-08-10 19:55:15 -0400

    D

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function myFunction() {
     element.setAttribute('aria-expanded', element.classList.contains('opened'));
     
     if(mls == false){
-        menu_list.style.display = "block"
+        menu_list.style.display = "flex"
         mls = true
     } else {
         menu_list.style.display = "none"
